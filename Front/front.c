670241cdd4b29793118e8fae59c922b35cc536f7
PixelTower <ohadra10@gmail.com>
2025-04-01 17:38:08 +0300

    change

--- Front/front.c
+++ Front/front.c
@@ -1,1 +1,30 @@
-/* filepath: /Users/ohadrahum/Documents/GettingStarted/MMN14/Front/front.c */
+#include "front.h"
+#include <string.h>
+#include <ctype.h>
+
+/* Array of valid opcodes, NULL terminated */
+static const char *valid_opcodes[] = {"mov", "cmp", "add", "sub", "lea", "not", "clr", "inc", "dec", "jmp", "bne", "red", "prn", "jsr", "rts", "stop", NULL};
+
+int validate_instruction(const char *instruction)
+{
+    char label[32], opcode[32], operands[128];
+    parse_instruction(instruction, label, opcode, operands);
+    return is_valid_instruction(opcode);
+}
+
+void parse_instruction(const char *line, char *label, char *opcode, char *operands)
+{
+    /* Basic parsing logic */
+    /* ...implementation here... */
+}
+
+int is_valid_instruction(const char *opcode)
+{
+    int i;
+    for (i = 0; valid_opcodes[i] != NULL; i++)
+    {
+        if (strcmp(valid_opcodes[i], opcode) == 0)
+            return 1;
+    }
+    return 0;
+}
--- Front/front.h
+++ Front/front.h
@@ -1,1 +1,15 @@
 /* filepath: /Users/ohadrahum/Documents/GettingStarted/MMN14/Front/front.h */
+
+#ifndef FRONT_H
+#define FRONT_H
+
+/* Function to validate assembly instruction syntax */
+int validate_instruction(const char *instruction);
+
+/* Function to parse assembly instruction into components */
+void parse_instruction(const char *line, char *label, char *opcode, char *operands);
+
+/* Function to check if the instruction is valid */
+int is_valid_instruction(const char *opcode);
+
+#endif /* FRONT_H */
