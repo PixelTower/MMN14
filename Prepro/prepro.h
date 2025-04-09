a7d56d3e992178876d38c1b00146e4188446c30e
Yuval Hadad <yuv2310.yh@gmail.com>
2025-04-09 21:16:02 +0200

    Update prepro.h

--- Prepro/prepro.h
+++ Prepro/prepro.h
@@ -9,22 +9,14 @@
 #define MAX_LABEL_LENGTH 31
 #define BIG_NUMBER_CONST 1000
 
-<<<<<<< HEAD
-/* Function declarations */
-
 /* Executes macro preprocessing on a cleaned file */
 node *macro_exec(const char *clean_file);
 
-=======
->>>>>>> 99d8934 (21:03)
 /* Adds a macro to the list */
 void add_to_list(node **head, const char *name, const char *content, int line);
 
 /* Searches for a macro by name */
 node *search_list(node *head, const char *name, int *found);
-
-/* Preprocess a file: handles macro expansion */
-FILE *preprocess_file(const char *filename);
 
 /* Clears macro list from memory */
 void free_macro_list(node *head);
