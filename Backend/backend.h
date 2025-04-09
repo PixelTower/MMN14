ce51e1a3e731204a621a4586963df4f7c1a95218
Yuval Hadad <yuv2310.yh@gmail.com>
2025-04-09 21:32:03 +0200

    Update backend.h

--- Backend/backend.h
+++ Backend/backend.h
@@ -8,4 +8,8 @@ int get_label_count(void);
 int find_label_address(const char *name);
 void free_label_table(label_struct *table, int count);
 
+label_struct *exe_first_pass(const char *filename, int *label_count, int *error_flag);
+void exe_second_pass(const char *filename, label_struct *table, int label_count,
+                     int *error_flag, int *extern_count, int *entry_count);
+
 #endif
